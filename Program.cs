f136e814ee917d77f3e1a2566e2c0f70a707e277
Денис <rogojin.deniska@gmail.com>
2023-04-19 10:29:27 +0300

    Создал метод для заполнения массива

--- Program.cs
+++ Program.cs
@@ -18,13 +18,37 @@ namespace qualExamPM03
         private App[] _apps;
 
 
-        //в конструкторе класса создается массив объектов типа App
+        //в конструкторе класса создается массив указателей на экземпляры типа App
         public Applications(int size)
         {
             _apps = new App[size];
         }
 
+
+        //метод для заполнения массива пользователем
+        public void FillArray()
+        {
+            for (int i = 0; i < _apps.Length; i++)
+            {
+                Console.Write($"Enter name of app {i + 1}: ");
+                string name = Console.ReadLine();
+                Console.Write($"Enter manufacturer of app {i + 1}: ");
+                string manufacturer = Console.ReadLine();
+                Console.Write($"Enter price of app {i + 1}: ");
+                int price = int.Parse(Console.ReadLine());
+
+                _apps[i] = new App
+                {
+                    Name = name,
+                    Manufacturer = manufacturer,
+                    Price = price
+                };
+            }
+        }
+
     }
+
+
     class Program
     {
         static void Main(string[] args)
