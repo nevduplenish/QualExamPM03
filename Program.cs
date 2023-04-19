b17c02fc88406c38009e21486e0047684c2cf2e9
Денис <rogojin.deniska@gmail.com>
2023-04-19 10:17:34 +0300

    Создал структуру App

--- Program.cs
+++ Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace qualExamPM03
 {
+    struct App
+    {
+        public string Name;
+        public string Manufacturer;
+        public int Price;
+    }
+
+    class Application 
+    {
+
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
